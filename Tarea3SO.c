e7ba58337b8e65ee56edf2ad9765c25277b1b72f
Jorge Caullan <jorge.caullan.12@sansano.usm.cl>
2015-12-26 23:44:56 -0300

    tarea 3 utilizando colas
    use colas de arreglos de tamaño maximo 10 y tuve que cambiar un monton de cosas

--- Tarea3SO.c
+++ Tarea3SO.c
@@ -11,88 +11,77 @@
 #include <sys/shm.h>
 
 #define MAXLINE 128
+#define MAXSIZE 10
 
 typedef struct{//Estructura utilizada para almacenar los datos del arreglo a utilizar con threads.
 	char cadena[MAXLINE];
 }linea;
 
-//IMPLEMENTACION DE LA COLA ********************************
+typedef struct { //cola de arreglos
+	unsigned int maxlen;
+	unsigned int len;
+	unsigned int pos_primero;
+	unsigned int pos_ultimo;
+	linea *elemento;
+}tcola;
 
-struct Node {
-	linea data;
-	struct Node* next;
-};
+// funciones colas
+void Initcola (tcola *C){
+	C->len=C->pos_primero=C->pos_ultimo=0;
+	C->maxlen=MAXSIZE;
+	C->elemento=(linea *)malloc(MAXSIZE*sizeof(linea));
+}
 
+int Enqueue(tcola *C, linea elemento){
+	if (C->len==C->maxlen){
+		return -1; //no hay espacio disponible
+	}
+	if (C->pos_ultimo==C->maxlen){
+		C->pos_ultimo=0;
+	}
+	C->elemento[C->pos_ultimo++]=elemento;
+	C->len++;
+	return 1; //inserccion exitosa
+}
+
+linea Dequeue(tcola *C){
+	linea aux;
+	if (C->len > 0){
+		aux=C->elemento[C->pos_primero];
+		if (C->pos_primero==C->maxlen) C->pos_primero=0;
+		C->pos_primero++;
+		C->len--;
+		return aux;
+	}
+	return aux;
+}
+
+void Clear(tcola *C){
+	C->len=C->pos_primero=C->pos_ultimo=0;
+	C->maxlen=MAXSIZE;
+	free((void *) C->elemento);
+}
+// fin funciones colas
+
+void* ret;
 pthread_mutex_t mutex; // MUTEX Global para que todos puedan acceder al mutex
+tcola colaspaces,colaupper,colawriter; //colas que se usaran
 
-// Two glboal variables to store address of front and rear nodes. 
-struct Node* front = NULL;
-struct Node* rear = NULL;
-
-// To Enqueue an integer
-void Enqueue(linea x) {
-	struct Node* temp = 
-		(struct Node*)malloc(sizeof(struct Node));
-	temp->data =x; 
-	temp->next = NULL;
-	if(front == NULL && rear == NULL){
-		front = rear = temp;
-		return;
-	}
-	rear->next = temp;
-	rear = temp;
-}
-
-// To Dequeue an integer.
-void Dequeue() {
-	struct Node* temp = front;
-	if(front == NULL) {
-		printf("Queue is Empty\n");
-		return;
-	}
-	if(front == rear) {
-		front = rear = NULL;
-	}
-	else {
-		front = front->next;
-	}
-	free(temp);
-}
-
-linea Front() {
-	if(front == NULL) {
-		printf("Queue is empty\n");
-		return front->data;
-	}
-	return front->data;
-}
-
-void Print() {
-	struct Node* temp = front;
-	while(temp != NULL) {
-		printf("%s ",temp->data.cadena);
-		temp = temp->next;
-	}
-	printf("\n");
-}
-
-
-//FIN DE IMPLEMENTACION DE LA COLA
-
-
-void *Writter (void *parametro) 
-{ 
+void *Writter (void * nada) { 
 	char line[MAXLINE];
-	strcpy(line,((linea *)parametro)->cadena);
+	linea recepcion;
+	recepcion=Dequeue(&colawriter);
+	strcpy(line,recepcion.cadena);
 	printf("%s", line);
+	//Clear(&colaspaces); Clear(&colaupper); Clear(&colawriter);
 	return NULL;
 } 
 
-void *Upper (void *parametro) 
-{ 
-	char line[MAXLINE];
-	strcpy(line,((linea *)parametro)->cadena);
-	char newline[MAXLINE];
+void *Upper (void * nada){ 
+	char line[MAXLINE], newline[MAXLINE];
+	linea recepcion, envio;
+	recepcion=Dequeue(&colaupper);
+	strcpy(line,recepcion.cadena);
 	int i = 0;
 	while(line[i]!='\0') 
 	{ 
@@ -101,65 +90,74 @@ void *Upper (void *parametro)
 		else newline[i]=line[i];
 		i++;
 	}
+    strcpy(envio.cadena,newline);
+    Enqueue(&colawriter, envio);
+    
+    //esto es para mandar un thread a writer
+//    pthread_mutex_init(&mutex, NULL);
+	pthread_t threadid;
+	pthread_create(&threadid, NULL, Writter, NULL);
 	
-	pthread_t threadid; //esto es para mandar un thread a writer
-	linea *parameter=(linea *)malloc(sizeof(linea));
-	strcpy(parameter->cadena,newline);
-	pthread_create(&threadid, NULL, Writter, parameter);
-
+//	pthread_join(threadid, &ret);
+//	pthread_mutex_destroy(&mutex);
 //	printf("%s",newline);
 	return NULL;
 } 
 
-void *spaces(void *parametro){
+void *spaces(void * nada){
 	int i;
-	char line[MAXLINE];
-	strcpy(line,((linea *)parametro)->cadena);
-	char newline[MAXLINE];
+	char line[MAXLINE], newline[MAXLINE];
+	linea recepcion, envio;
+	recepcion=Dequeue(&colaspaces);
+	strcpy(line,recepcion.cadena);
 	for(i=0;i<=strlen(line);i++){
 		if(line[i]==' ') newline[i]='*';
 		else newline[i]=line[i];
 	}
+    strcpy(envio.cadena,newline);
+    Enqueue(&colaupper, envio);
 	//enviar nuevalinea a un thread upper
+//	pthread_mutex_init(&mutex, NULL);
 	pthread_t threadid;
-	linea *parameter=(linea *)malloc(sizeof(linea));
-	strcpy(parameter->cadena,newline);
-	pthread_create(&threadid, NULL, Upper, parameter);
+	pthread_create(&threadid, NULL, Upper, NULL);
+	
+//	pthread_join(threadid, &ret);
+//	pthread_mutex_destroy(&mutex); // Desinicializa el mutex
 //	printf("%s", newline);
 	return NULL;
 }
 
 void *reader(void * nada){
 	char line[MAXLINE];
+	linea envios;
 	FILE *archivo;
 	archivo= fopen("unarchivo.txt","r");
+	Initcola(&colaspaces);
+	Initcola(&colaupper);
+	Initcola(&colawriter);
 	if (archivo==NULL){
 		printf("el archivo \"unarchivo.txt\" no se encuentra en el directorio");
 		return NULL;
     }
 	while(fgets(line, MAXLINE, archivo)!=NULL){
-		
 		pthread_attr_t attr;
-	    void* ret;
-	    pthread_mutex_init(&mutex, NULL); // Inicializamos el mutex
 	    pthread_attr_init(&attr);
 	    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+		pthread_mutex_init(&mutex, NULL); // Inicializamos el mutex
 
         //aqui mandar la linea a un thread que la modifique
+        strcpy(envios.cadena,line);
+        Enqueue(&colaspaces, envios);
+        
         pthread_t threadid;
-		linea *parametro=(linea *)malloc(sizeof(linea));
-		strcpy(parametro->cadena,line);
-		pthread_create(&threadid, NULL, spaces, parametro);
-
+		pthread_create(&threadid, NULL, spaces, NULL);
 
 		pthread_join(threadid, &ret);
 	    pthread_attr_destroy(&attr); // Borramos de la memoria los atributos
 
-	    /* Antes de llamar a esta función TODOS los threads que usen el mutex
-      tienen que estar cerrados. */
-   		pthread_mutex_destroy(&mutex); // Desinicializa el mutex
+	    /* Antes de llamar a esta función TODOS los threads que usen el mutex tienen que estar cerrados. */
+		pthread_mutex_destroy(&mutex); // Desinicializa el mutex
 
-   		
     }
     pthread_exit(NULL);
 //        spaces(line);
@@ -170,10 +168,36 @@ void *reader(void * nada){
 
 int main (int argc, char *argv[])
 {
+	Initcola(&colaupper);
+	Initcola(&colawriter);
 	pthread_t threadid;
-	pthread_create(&threadid, NULL, reader, NULL);	
+	pthread_create(&threadid, NULL, reader, NULL);
 	pthread_exit(NULL);
+	
+	
+	
+/*	tcola C1,C2,C3;
+	Initcola(&C1);
+	Initcola(&C2);
+	Initcola(&C3);
+	
+	//elementos
+	linea asd1;	linea asd2;	linea asd3;	linea asd4;
+	strcpy(asd1.cadena,"dato1"); strcpy(asd2.cadena,"dato2"); strcpy(asd3.cadena,"dato3"); strcpy(asd4.cadena,"dato4");
+	
+	Enqueue(&C1, asd1);
+	Enqueue(&C1, asd2);
+	Enqueue(&C2, asd3);
+	Enqueue(&C3, asd4);
+	linea dasd=Dequeue(&C1);
+	printf("%s\n",dasd.cadena);
 
+	dasd=Dequeue(&C2);
+	printf("%s\n",dasd.cadena);
+	
+	Clear(&C1);
+	Clear(&C2);
+	Clear(&C3);*/
+	
+	return 0;
 }
-
-
